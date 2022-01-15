314ce3f14834827dce0147633809498a74bfe2cd
Nick Kornienko <nkornien@ucsc.edu>
2022-01-14 19:19:08 -0800

    fixed operator+

--- code/ubigint.cpp
+++ code/ubigint.cpp
@@ -11,50 +11,58 @@ using namespace std;
 #include "debug.h"
 #include "ubigint.h"
 
-ostream& operator<< (ostream& out, const ubigint::ubigvalue_t& uvalue) {
-   for (const auto& digit: uvalue) out << static_cast<int> (digit);
+ostream &operator<<(ostream &out, const ubigint::ubigvalue_t &uvalue)
+{
+   for (const auto &digit : uvalue)
+      out << static_cast<int>(digit);
    return out;
 }
 
-ubigint::ubigint (unsigned long that): uvalue (that) {
-   DEBUGF ('~', this << " -> " << uvalue)
+ubigint::ubigint(unsigned long that) : uvalue(that)
+{
 }
 
-ubigint::ubigint (const string& that): uvalue(0) {
-   for (char digit: that) {
-      if (not isdigit (digit)) {
-         throw invalid_argument ("ubigint::ubigint(" + that + ")");
+ubigint::ubigint(const string &that) : uvalue(0)
+{
+   for (char digit : that)
+   {
+      if (not isdigit(digit))
+      {
+         throw invalid_argument("ubigint::ubigint(" + that + ")");
       }
       uvalue.push_back(digit - '0');
    }
 }
 
-void ubigint::pad_zeros (vector<uint8_t>* vec1,
- vector<uint8_t>* vec2) const
+void ubigint::pad_zeros(vector<uint8_t> *vec1,
+                        vector<uint8_t> *vec2) const
 {
-    if (vec1->size() == vec2->size())
-       return;
-    else 
-    {
-       int diff = abs(static_cast<int>(vec1->size()) -
-        static_cast<int>(vec2->size()));
+   if (vec1->size() == vec2->size())
+   {
+      return;
+   }
 
-       if (vec1->size() > vec2->size())
-       {
-         for(int i = 0; i < diff; i++)
-            vec2->push_back(0);
-         reverse(vec2->begin(),vec2->end());
-       }
-       else
-       {
-         for(int i = 0; i < diff; i++)
-            vec1->push_back(0);
-         reverse(vec1->begin(),vec1->end());
-       }
-    }
+   int diff = abs(static_cast<int>(vec1->size()) -
+                  static_cast<int>(vec2->size()));
+   if (vec1->size() > vec2->size())
+   {
+      for (int i = 0; i < diff; i++)
+      {
+         vec2->push_back(0);
+      }
+      // reverse(vec2->begin(), vec2->end());
+   }
+   else
+   {
+      for (int i = 0; i < diff; i++)
+      {
+         vec1->push_back(0);
+      }
+      // reverse(vec1->begin(), vec1->end());
+   }
 }
 
-void ubigint::trim_zeros (vector<uint8_t>* vec) const
+void ubigint::trim_zeros(vector<uint8_t> *vec) const
 {
    while (vec->size() > 0 && vec->back() == 0)
    {
@@ -62,15 +70,27 @@ void ubigint::trim_zeros (vector<uint8_t>* vec) const
    }
 }
 
-ubigint ubigint::operator+ (const ubigint& that) const {
-  
+ubigint ubigint::operator+(const ubigint &that) const
+{
+
    vector<uint8_t> vecL = this->uvalue;
    vector<uint8_t> vecR = that.uvalue;
    vector<uint8_t> result;
-   pad_zeros(&vecL, &vecR);
+   reverse(vecL.begin(), vecL.end());
+   reverse(vecR.begin(), vecR.end());
 
-   reverse(vecL.begin(),vecL.end());
-   reverse(vecR.begin(),vecR.end());
+   pad_zeros(&vecL, &vecR);
+   
+   // for (size_t i = 0; i < vecL.size(); i++)
+   // {
+   //    cout << static_cast<int>(vecL[i]);
+   // }
+   // cout << "\n";
+   // for (size_t i = 0; i < vecR.size(); i++)
+   // {
+   //    cout << static_cast<int>(vecR[i]);
+   // }
+   // cout << "\n";
 
    result.push_back(0);
    for (size_t i = 0; i < vecL.size(); i++)
@@ -83,93 +103,110 @@ ubigint ubigint::operator+ (const ubigint& that) const {
       result.push_back(carry);
    }
    trim_zeros(&result);
-   reverse(result.begin(),result.end());
+   reverse(result.begin(), result.end());
    ubigint retval;
    retval.uvalue = result;
 
    return retval;
 }
 
-ubigint ubigint::operator- (const ubigint& that) const {
-   DEBUGF ('u', *this << "+" << that);
+ubigint ubigint::operator-(const ubigint &that) const
+{
+   DEBUGF('u', *this << "+" << that);
    ubigint result;
-/***FIXME***
+   /***FIXME***
    if (*this < that) throw domain_error ("ubigint::operator-(a<b)");
    return ubigint (uvalue - that.uvalue);
 ***/
    return result;
 }
 
-ubigint ubigint::operator* (const ubigint& that) const {
-   DEBUGF ('u', *this << "+" << that);
+ubigint ubigint::operator*(const ubigint &that) const
+{
+   DEBUGF('u', *this << "+" << that);
    ubigint result;
-/***FIXME***
+   /***FIXME***
    return ubigint (uvalue * that.uvalue);
 ***/
    return result;
 }
 
-void ubigint::multiply_by_2() {
-/***FIXME***
+void ubigint::multiply_by_2()
+{
+   /***FIXME***
    uvalue *= 2;
 ***/
 }
 
-void ubigint::divide_by_2() {
-/***FIXME***
+void ubigint::divide_by_2()
+{
+   /***FIXME***
    uvalue /= 2;
 ***/
 }
 
-
-struct quo_rem { ubigint quotient; ubigint remainder; };
-quo_rem udivide (const ubigint& dividend, const ubigint& divisor_) {
+struct quo_rem
+{
+   ubigint quotient;
+   ubigint remainder;
+};
+quo_rem udivide(const ubigint &dividend, const ubigint &divisor_)
+{
    // NOTE: udivide is a non-member function.
-   ubigint divisor {divisor_};
-   ubigint zero {0};
-   if (divisor == zero) throw domain_error ("udivide by zero");
-   ubigint power_of_2 {1};
-   ubigint quotient {0};
-   ubigint remainder {dividend}; // left operand, dividend
-   while (divisor < remainder) {
+   ubigint divisor{divisor_};
+   ubigint zero{0};
+   if (divisor == zero)
+      throw domain_error("udivide by zero");
+   ubigint power_of_2{1};
+   ubigint quotient{0};
+   ubigint remainder{dividend}; // left operand, dividend
+   while (divisor < remainder)
+   {
       divisor.multiply_by_2();
       power_of_2.multiply_by_2();
    }
-   while (power_of_2 > zero) {
-      if (divisor <= remainder) {
+   while (power_of_2 > zero)
+   {
+      if (divisor <= remainder)
+      {
          remainder = remainder - divisor;
          quotient = quotient + power_of_2;
       }
       divisor.divide_by_2();
       power_of_2.divide_by_2();
    }
-   DEBUGF ('/', "quotient = " << quotient);
-   DEBUGF ('/', "remainder = " << remainder);
+   DEBUGF('/', "quotient = " << quotient);
+   DEBUGF('/', "remainder = " << remainder);
    return {.quotient = quotient, .remainder = remainder};
 }
 
-ubigint ubigint::operator/ (const ubigint& that) const {
-   return udivide (*this, that).quotient;
+ubigint ubigint::operator/(const ubigint &that) const
+{
+   return udivide(*this, that).quotient;
 }
 
-ubigint ubigint::operator% (const ubigint& that) const {
-   return udivide (*this, that).remainder;
+ubigint ubigint::operator%(const ubigint &that) const
+{
+   return udivide(*this, that).remainder;
 }
 
-bool ubigint::operator== (const ubigint& that) const {
+bool ubigint::operator==(const ubigint &that) const
+{
    return uvalue == that.uvalue;
 }
 
-bool ubigint::operator< (const ubigint& that) const {
+bool ubigint::operator<(const ubigint &that) const
+{
    return uvalue < that.uvalue;
 }
 
-void ubigint::print() const {
-   DEBUGF ('p', this << " -> " << *this);
+void ubigint::print() const
+{
+   DEBUGF('p', this << " -> " << *this);
    cout << uvalue;
 }
 
-ostream& operator<< (ostream& out, const ubigint& that) { 
+ostream &operator<<(ostream &out, const ubigint &that)
+{
    return out << "ubigint(" << that.uvalue << ")";
 }
-
