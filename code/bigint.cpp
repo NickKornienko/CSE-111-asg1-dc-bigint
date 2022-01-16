369676d457843a6ffc16969ac096051aa437e41c
James Andrew Garrett <jaagarre@unix4.lt.ucsc.edu>
2022-01-16 15:53:32 -0800

    operator+ bug fixes

--- code/bigint.cpp
+++ code/bigint.cpp
@@ -43,7 +43,7 @@ bigint bigint::operator+(const bigint &that) const
    }
    else
    {
-      if (*this < that)
+      if (this->uvalue < that.uvalue)
       {
          result.uvalue = that.uvalue - this->uvalue;
          result.is_negative = that.is_negative;
